5b19590a2967c3160674e0f241dbdb0589695246
Atena Jafari Parsa <atena.jafariparsa@bahcesehir.edu.tr>
2025-11-10 15:53:27 +0300

    cleaned app.py

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def certifications():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use Render’s port or default to 5000 locally
+    port = int(os.environ.get("PORT", 5000))  
     app.run(host="0.0.0.0", port=port)
