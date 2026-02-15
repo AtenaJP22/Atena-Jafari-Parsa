1d73d173f6b04e26c8074318c20b9f851cf4093b
Atena Jafari Parsa <atena.jafariparsa@bahcesehir.edu.tr>
2026-02-15 15:52:34 +0300

    language switch universal

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def certifications():
     return render_template("certifications.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
+    port = int(os.environ.get("PORT", 5001))  
     app.run(host="0.0.0.0", port=port)
